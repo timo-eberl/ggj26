9e2c01f06f71efcad1d9f4513dbf5f414acde695
as439 <as439@hdm-stuttgart.de>
2026-01-30 23:43:04 +0100

    added player controller and camera movement

---
+++ scripts/player/playerController.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export var MOUSE_SENSITIVITY: float = 0.5
+@export var TILT_LOWER_LIMIT : float = deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT : float = deg_to_rad(90.0)
+@export var CAMERA_CONTROLLER: Camera3D
+
+var _mouse_input: bool = false
+var _mouse_rotation: Vector3
+var _rotation_input: float = 0.0
+var _tilt_input: float = 0.0
+var _player_rotation: Vector3
+var _camera_rotation: Vector3
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	_update_camera(delta)
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	move_and_slide()
+
+
+func _update_camera(delta: float) -> void:
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+
+	_player_rotation = Vector3(0.0, _mouse_rotation.y, 0.0)
+	_camera_rotation = Vector3(_mouse_rotation.x, 0.0, 0.0)
+
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0
+
+	global_transform.basis = Basis.from_euler(_player_rotation)
+
+	_rotation_input = 0.0
+	_tilt_input = 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	
+	if _mouse_input:
+		_rotation_input = -event.relative.x
+		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
