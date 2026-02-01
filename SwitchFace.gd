0163554e0110753fe0c81794fcb11b6e985139b4
lr080 <lr080@hdm-stuttgart.de>
2026-02-01 02:52:30 +0100

    face Swap

--- SwitchFace.gd
+++ SwitchFace.gd
@@ -3,10 +3,7 @@ extends MeshInstance3D
 @export var faces : Array[Texture2D]
 
 func _ready():
-	var material: ShaderMaterial = load("res://assets/materials/Gesichtermaterial.tres")
-		
-	
+	var material = material_override.duplicate()
+	material_override = material
 	material_override.set_shader_parameter("tex_frg_2", faces[randi_range(0,8)])
-	
-	set_surface_override_material(0, material)
 	#material_override = material
